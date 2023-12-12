e447971040612f2f437c69134219dbcf554d8e47
rbgksqkr <rbgks1937@naver.com>
2023-12-13 01:45:50 +0900

    refactor: 필요없는 필드값 제거

--- src/Coach.js
+++ src/Coach.js
@@ -1,30 +1,20 @@
 class Coach {
-  name = '';
+  name;
   notEatMenus = [];
   eatMenus = [];
-  recommendedMenus = [];
 
   constructor(name) {
-    this.name = name;
+    this.#name = name;
   }
 
   addNotEatMenu(menu) {
     this.notEatMenus.push(menu);
   }
 
-  addRecommendMenu(menu) {
-    this.recommendedMenus.push(menu);
-  }
-
   addEatMenu(menu) {
     this.eatMenus.push(menu);
   }
 
-  checkValidRecommendMenu(menu) {
-    if (this.notEatMenus.includes(menu)) {
-      throw '[ERROR] 중복';
-    }
-  }
   printMenu() {
     return this.eatMenus.join(' | ');
   }
